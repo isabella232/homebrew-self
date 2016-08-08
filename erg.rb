9eb5986c6b3cae73f50d9af7b9e3e6c2251af090
Andrew Lazarus <nerdrew@gmail.com>
2016-08-08 09:33:57 -0700

    fix deprecations

--- erg.rb
+++ erg.rb
@@ -4,7 +4,7 @@ class Erg < Formula
   homepage "https://github.com/square/erg"
   url "https://github.com/square/erg/archive/v1.1.1.tar.gz"
   head "https://github.com/square/erg.git"
-  sha1 "95b3935557a47703998f6aede1e549459f69275d"
+  sha256 "95b3935557a47703998f6aede1e549459f69275d"
 
   depends_on 'go' => :build
 
@@ -20,7 +20,7 @@ class Erg < Formula
   bottle do
     root_url 'https://raw.githubusercontent.com/square/homebrew-bottles/master'
     cellar :any
-    sha1 "5d8da89bddeb5b0a8b280469c406973ad31c895e" => :mavericks
+    sha256 "5d8da89bddeb5b0a8b280469c406973ad31c895e" => :mavericks
     sha256 "59dab808928a2a5d22f09b3c85563b474efdeabfc8445430323eb720dc4ebdc9" => :yosemite
     sha256 "fc3f255620471bdeb8cd7e73ec7aafd89f27be0e491931bc1a0b9fde2c660e81" => :el_capitan
   end
--- gssh.rb
+++ gssh.rb
@@ -4,7 +4,7 @@ class Gssh < Formula
   homepage "https://github.com/square/gssh"
   url "https://github.com/square/gssh/archive/v1.0.1.tar.gz"
   head "https://github.com/square/gssh.git"
-  sha1 "db2d9b087638838f836c8ad9cf63014bbfeefc85"
+  sha256 "db2d9b087638838f836c8ad9cf63014bbfeefc85"
 
   depends_on 'go' => :build
 
@@ -18,6 +18,6 @@ class Gssh < Formula
   bottle do
     root_url 'https://raw.githubusercontent.com/square/homebrew-bottles/master'
     cellar :any
-    sha1 "b329fe9b2f8248b2854b3eb0ebed16a52e24292c" => :mavericks
+    sha256 "b329fe9b2f8248b2854b3eb0ebed16a52e24292c" => :mavericks
   end
 end
